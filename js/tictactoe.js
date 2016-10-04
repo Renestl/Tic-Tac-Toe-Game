02cc795baba07e81b57d740c0addd55db84041a5
Jennifer Currie <renestl@gmail.com>
2016-10-04 15:22:51 -0500

    working two player game

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,49 +1,35 @@
 // Code goes here
 
-// assign the players
-var player,
-  computer,
+// assign players and beginning score
+var player = "X",
+  computer = "O",
   moves = 0,
   currentPlayer,
-  currentPlayerMark = 'X',
+  // currentPlayer,
   playerScore = 0,
   computerScore = 0,
   drawScore = 0,
   gameOver = false,
-  isDraw = false,
-  tableClickable = true;
+  isDraw = false;
 
 // sets the player if button is clicked
-// 'X' always goes first
 $('button').click(function() {
   if ($(this).attr('id') === ('xButton')) {
     player = "X";
-    computer = 'O';
-    currentPlayer = player;
-    // fadeMsg();
+  	computer = "O";
+  	currentPlayer = player;
   } else if ($(this).attr('id') === ('oButton')) {
     player = "O";
-    computer = 'X';
-    currentPlayer = computer;
-    // fadeMsg();
+  	computer = "X";
+  	currentPlayer = computer;
     // compFirst();
   }
 });
 
-//TODO: fadeMsg(); 
-// fade in message
-// 'Start Us Off!' (for player = X)
-// 'Computer Starts!' (for computer = X)
-// fade out message
-
-// TODO: compFirst()
-//allows the computer to make the first move
-
-
 // enters player letter, toggles to next player
 $('td').click(function() {
   if ($(this).text() === '') {
-    $(this).text(currentPlayerMark);
+    $(this).text(currentPlayer);
     moves++;
     checkWin();
     togglePlayer();
@@ -54,193 +40,112 @@ $('td').click(function() {
 
 // toggles X and O between turns
 function togglePlayer() {
-  if (currentPlayerMark === "X") {
-    currentPlayerMark = "O";
+  if (currentPlayer === player) {
+    currentPlayer = computer;
   } else {
-    currentPlayerMark = "X";
+    currentPlayer = player;
   }
 }
 
 // Check for a win
 function checkWin() {
   switch (true) {
-    case $('#c1r1').text() === currentPlayerMark && $('#c2r1').text() === currentPlayerMark && $('#c3r1').text() === currentPlayerMark:
+    case $('#c1r1').text() === currentPlayer && $('#c2r1').text() === currentPlayer && $('#c3r1').text() === currentPlayer:
       DrawLine('#c1r1', '#c2r1', '#c3r1');
       gameOver = true;
-      setTimeout(updateScore, 1000);
-      // setTimeout(reset, 1000);
+      gameEnd();
       break;
-    case $('#c1r2').text() === currentPlayerMark && $('#c2r2').text() === currentPlayerMark && $('#c3r2').text() === currentPlayerMark:
+    case $('#c1r2').text() === currentPlayer && $('#c2r2').text() === currentPlayer && $('#c3r2').text() === currentPlayer:
       DrawLine('#c1r2', '#c2r2', '#c3r2');
       gameOver = true;
-      setTimeout(updateScore, 1000);
-      // setTimeout(reset, 1000);
+      gameEnd();
       break;
-    case $('#c1r3').text() === currentPlayerMark && $('#c2r3').text() === currentPlayerMark && $('#c3r3').text() === currentPlayerMark:
+    case $('#c1r3').text() === currentPlayer && $('#c2r3').text() === currentPlayer && $('#c3r3').text() === currentPlayer:
       DrawLine('#c1r3', '#c2r3', '#c3r3');
       gameOver = true;
-      setTimeout(updateScore, 1000);
-      // setTimeout(reset, 1000);
+      gameEnd();
       break;
-    case $('#c1r1').text() === currentPlayerMark && $('#c1r2').text() === currentPlayerMark && $('#c1r3').text() === currentPlayerMark:
+    case $('#c1r1').text() === currentPlayer && $('#c1r2').text() === currentPlayer && $('#c1r3').text() === currentPlayer:
       DrawLine('#c1r1', '#c1r2', '#c1r3');
       gameOver = true;
-      setTimeout(updateScore, 1000);
-      // setTimeout(reset, 1000);
+      gameEnd();
       break;
-    case $('#c2r1').text() === currentPlayerMark && $('#c2r2').text() === currentPlayerMark && $('#c2r3').text() === currentPlayerMark:
+    case $('#c2r1').text() === currentPlayer && $('#c2r2').text() === currentPlayer && $('#c2r3').text() === currentPlayer:
       DrawLine('#c2r1', '#c2r2', '#c2r3');
       gameOver = true;
-      setTimeout(updateScore, 1000);
-      // setTimeout(reset, 1000);
+      gameEnd();
       break;
-    case $('#c3r1').text() === currentPlayerMark && $('#c3r2').text() === currentPlayerMark && $('#c3r3').text() === currentPlayerMark:
+    case $('#c3r1').text() === currentPlayer && $('#c3r2').text() === currentPlayer && $('#c3r3').text() === currentPlayer:
       DrawLine('#c3r1', '#c3r2', '#c3r3');
       gameOver = true;
-      setTimeout(updateScore, 1000);
-      // setTimeout(reset, 1000);
+      gameEnd();
       break;
-    case $('#c1r1').text() === currentPlayerMark && $('#c2r2').text() === currentPlayerMark && $('#c3r3').text() === currentPlayerMark:
+    case $('#c1r1').text() === currentPlayer && $('#c2r2').text() === currentPlayer && $('#c3r3').text() === currentPlayer:
       DrawLine('#c1r1', '#c2r2', '#c3r3');
       gameOver = true;
-      setTimeout(updateScore, 1000);
-      // setTimeout(reset, 1000);
+      gameEnd();
       break;
-    case $('#c3r1').text() === currentPlayerMark && $('#c2r2').text() === currentPlayerMark && $('#c1r3').text() === currentPlayerMark:
+    case $('#c3r1').text() === currentPlayer && $('#c2r2').text() === currentPlayer && $('#c1r3').text() === currentPlayer:
       DrawLine('#c3r1', '#c2r2', '#c1r3');
       gameOver = true;
-      setTimeout(updateScore, 1000);
-      // setTimeout(reset, 1000);
+      gameEnd();
       break;
     default:
       checkDraw();
-      
-      setTimeout(updateScore, 1000);
-      // setTimeout(reset, 1000);
   }
 }
 
 // draw line from player win
-function DrawLine(pos1, pos2, pos3) {
-  var $pos1 = $(pos1);
-  var $pos2 = $(pos2);
-  var $pos3 = $(pos3);
-  $pos1.addClass('playWin');
-  $pos2.addClass('playWin');
-  $pos3.addClass('playWin');
+function DrawLine(tdPos1, tdPos2, tdPos3) {
+  var $tdPos1 = $(tdPos1);
+  var $tdPos2 = $(tdPos2);
+  var $tdPos3 = $(tdPos3);
+  $tdPos1.addClass('playWin');
+  $tdPos2.addClass('playWin');
+  $tdPos3.addClass('playWin');
 
   togglePlayer(currentPlayer);
 }
 
-// TODO: Check for draw
+// Check for draw
 function checkDraw() {
-  if (moves === 9) {
-    gameOver === true;
+  if (moves >= 9) {
+    gameOver = true;
     isDraw = true;
-    isClickable(gameOver);
-    //setTimeout(reset, 1000);
-
-    //TODO: popup stating draw
+    $('td').addClass('gameDraw');
+    gameEnd();
   }
 }
 
-// TODO: update score function
+function gameEnd() {
+  setTimeout(updateScore, 1000);
+  setTimeout(reset, 2000);
+}
+
+// update score function
 function updateScore() {
   if (gameOver === true && isDraw === true) {
-    console.log(drawScore);
     drawScore++;
-    console.log(drawScore);
-    // } else if (gameOver === true && isDraw === false) {
-    //   if (currentPlayerMark === "X") {
-    //     if (currentPlayer === player) {
-    //       player++;
-    //     } else {
-    //       computer++;
-    //     }
-    //   } else if (currentPlayerMark === "O") {
-    //     if (currentPlayer === player) {
-    //       player++;
-    //     } else {
-    //       computer++;
-    //     }
-    //   }
-  }
+    $('.draw').text(drawScore);
+    } 
+    else if (gameOver === true && isDraw === false && currentPlayer === player) {
+    playerScore++;
+    $('.player').text(playerScore);
+    } 
+    else if (gameOver === true && isDraw === false && currentPlayer === computer) {
+    computerScore++;
+    $('.computer').text(computerScore);
+    }
 }
 
-
-
-// TODO: make table unclickable
-function isClickable(gameOver) {
-  if (gameOver === true) {
-    $('#field').prop('disabled', true);
-  }
+// reset function
+function reset() {
+	$('td').text("");
+	gameOver = false;
+	isDraw = false;
+	moves = 0;
+	$('td').removeClass('playWin');
+  $('td').removeClass('gameDraw');
 }
 
-// TODO: reset function
-// clear board
-
-// reset game
-
-
-// other variable
-// var currentPlayer = 'X',
-
-//   playerScore = 0,
-//   computerScore = 0,
-//   cellNum,
-//   cellPosition = [];
-
-// // sets the player if button is clicked
-// var assignment = {
-//   xButton: function() {
-//     player = 'X';
-//     computer = 'O';
-//   },
-//   oButton: function() {
-//     player = 'O';
-//     computer = 'X';
-//   }
-// };
-
-
-// function chooseSquare {
-
-// }
-
-// function chooseSpace() {
-//   for (i = 0; i < spaces.length; i++) {
-//   if () 
-//   }
-
-// }
-
-
-
-// // Declare winner
-
-// // Increase score after win
-// function updateScore() {
-//   if (playerWin === true) {
-//     playerScore++;
-//   } else {
-//     computerScore++;
-//   }
-//   Clear();
-// }
-
-// // Clear board after win
-// function clear() {
-// clear board
-
-// allow board to be clickable again
-
-//clear winner line
-
-//reset moves
-
-//reset gameover
-
-// reset player to chosen one
-//}
-
-//Mark position clicked
+// TODO: computer AI
