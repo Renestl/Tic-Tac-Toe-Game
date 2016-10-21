e19c57b0b353335c4604a4dc1444e3d3ff08448f
Jennifer Currie <renestl@gmail.com>
2016-10-21 04:47:08 -0500

    completed working computer logic

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,48 +1,62 @@
 // Code goes here
 
 // assign players and beginning score
-var player = "X",
-  computer = "O",
+var player,
+  computer,
   moves = 0,
   currentPlayer,
-  // currentPlayer,
   playerScore = 0,
   computerScore = 0,
   drawScore = 0,
   gameOver = false,
-  isDraw = false;
+  isDraw = false,
+  gameNum = 0;
 
 // sets the player if button is clicked
-$('button').click(function() {
-  if ($(this).attr('id') === ('xButton')) {
-    player = "X";
-  	computer = "O";
-  	currentPlayer = player;
-  } else if ($(this).attr('id') === ('oButton')) {
-    player = "O";
-  	computer = "X";
-  	currentPlayer = computer;
-    // compFirst();
-  }
+
+  $('button').click(function() {
+   
+    if ($(this).attr('id') === ('xButton')) {
+      player = "X";
+    	computer = "O";
+    	currentPlayer = player;
+    } else if ($(this).attr('id') === ('oButton')) {
+      player = "O";
+    	computer = "X";
+    	currentPlayer = player;
+    } 
+  });
+
+
+// registers which cell was clicked
+$('td').click(function() {
+  var id = $(this).attr('id');
+  placeMark(id);
+  allChecks();
 });
 
-// enters player letter, toggles to next player
-$('td').click(function() {
-  if ($(this).text() === '') {
-    $(this).text(currentPlayer);
-    moves++;
-    checkWin();
-    togglePlayer();
-  } else {
-    alert('space taken!');
+// steps for both player and computer to take and propel game forward
+function allChecks() {
+  moves++;
+  checkWin();
+  togglePlayer();
+}
+
+// enters player letter
+function placeMark(id) {
+if ($(this).text() === '') {
+	$('#' + id).html(currentPlayer);
+} else {
+  alert('space taken!');
   }
-});
+}
 
 // toggles X and O between turns
 function togglePlayer() {
   if (currentPlayer === player) {
     currentPlayer = computer;
-  } else {
+    computerMove();
+  } else if(currentPlayer === computer) {
     currentPlayer = player;
   }
 }
@@ -50,43 +64,43 @@ function togglePlayer() {
 // Check for a win
 function checkWin() {
   switch (true) {
-    case $('#c1r1').text() === currentPlayer && $('#c2r1').text() === currentPlayer && $('#c3r1').text() === currentPlayer:
-      DrawLine('#c1r1', '#c2r1', '#c3r1');
+    case $('#1').text() === currentPlayer && $('#2').text() === currentPlayer && $('#3').text() === currentPlayer:
+      DrawLine('#1', '#2', '#3');
       gameOver = true;
       gameEnd();
       break;
-    case $('#c1r2').text() === currentPlayer && $('#c2r2').text() === currentPlayer && $('#c3r2').text() === currentPlayer:
-      DrawLine('#c1r2', '#c2r2', '#c3r2');
+    case $('#4').text() === currentPlayer && $('#5').text() === currentPlayer && $('#6').text() === currentPlayer:
+      DrawLine('#4', '#5', '#6');
       gameOver = true;
       gameEnd();
       break;
-    case $('#c1r3').text() === currentPlayer && $('#c2r3').text() === currentPlayer && $('#c3r3').text() === currentPlayer:
-      DrawLine('#c1r3', '#c2r3', '#c3r3');
+    case $('#7').text() === currentPlayer && $('#8').text() === currentPlayer && $('#9').text() === currentPlayer:
+      DrawLine('#7', '#8', '#9');
       gameOver = true;
       gameEnd();
       break;
-    case $('#c1r1').text() === currentPlayer && $('#c1r2').text() === currentPlayer && $('#c1r3').text() === currentPlayer:
-      DrawLine('#c1r1', '#c1r2', '#c1r3');
+    case $('#1').text() === currentPlayer && $('#4').text() === currentPlayer && $('#7').text() === currentPlayer:
+      DrawLine('#1', '#4', '#7');
       gameOver = true;
       gameEnd();
       break;
-    case $('#c2r1').text() === currentPlayer && $('#c2r2').text() === currentPlayer && $('#c2r3').text() === currentPlayer:
-      DrawLine('#c2r1', '#c2r2', '#c2r3');
+    case $('#2').text() === currentPlayer && $('#5').text() === currentPlayer && $('#8').text() === currentPlayer:
+      DrawLine('#2', '#5', '#8');
       gameOver = true;
       gameEnd();
       break;
-    case $('#c3r1').text() === currentPlayer && $('#c3r2').text() === currentPlayer && $('#c3r3').text() === currentPlayer:
-      DrawLine('#c3r1', '#c3r2', '#c3r3');
+    case $('#3').text() === currentPlayer && $('#6').text() === currentPlayer && $('#9').text() === currentPlayer:
+      DrawLine('#3', '#6', '#9');
       gameOver = true;
       gameEnd();
       break;
-    case $('#c1r1').text() === currentPlayer && $('#c2r2').text() === currentPlayer && $('#c3r3').text() === currentPlayer:
-      DrawLine('#c1r1', '#c2r2', '#c3r3');
+    case $('#1').text() === currentPlayer && $('#5').text() === currentPlayer && $('#9').text() === currentPlayer:
+      DrawLine('#1', '#5', '#9');
       gameOver = true;
       gameEnd();
       break;
-    case $('#c3r1').text() === currentPlayer && $('#c2r2').text() === currentPlayer && $('#c1r3').text() === currentPlayer:
-      DrawLine('#c3r1', '#c2r2', '#c1r3');
+    case $('#3').text() === currentPlayer && $('#5').text() === currentPlayer && $('#7').text() === currentPlayer:
+      DrawLine('#3', '#5', '#7');
       gameOver = true;
       gameEnd();
       break;
@@ -103,8 +117,6 @@ function DrawLine(tdPos1, tdPos2, tdPos3) {
   $tdPos1.addClass('playWin');
   $tdPos2.addClass('playWin');
   $tdPos3.addClass('playWin');
-
-  togglePlayer(currentPlayer);
 }
 
 // Check for draw
@@ -118,6 +130,7 @@ function checkDraw() {
 }
 
 function gameEnd() {
+  gameNum++;
   setTimeout(updateScore, 1000);
   setTimeout(reset, 2000);
 }
@@ -128,11 +141,11 @@ function updateScore() {
     drawScore++;
     $('.draw').text(drawScore);
     } 
-    else if (gameOver === true && isDraw === false && currentPlayer === player) {
+    else if (gameOver === true && isDraw === false && currentPlayer === computer) {
     playerScore++;
     $('.player').text(playerScore);
     } 
-    else if (gameOver === true && isDraw === false && currentPlayer === computer) {
+    else if (gameOver === true && isDraw === false && currentPlayer === player) {
     computerScore++;
     $('.computer').text(computerScore);
     }
@@ -146,6 +159,45 @@ function reset() {
 	moves = 0;
 	$('td').removeClass('playWin');
   $('td').removeClass('gameDraw');
+  togglePlayer();
 }
 
 // TODO: computer AI
+function computerMove() {
+  if (gameOver === false && isDraw === false) {
+    computerAI();
+    allChecks();
+  }
+}
+
+function computerAI() {
+  switch (true) {
+    case $('#1').text() !== player && $('#1').text() !== computer:
+     placeMark('1');
+      break;
+    case $('#2').text() !== player && $('#2').text() !== computer:
+     placeMark('2');
+      break;
+    case $('#3').text() !== player && $('#3').text() !== computer:
+      placeMark('3');
+      break;
+    case $('#4').text() !== player && $('#4').text() !== computer:
+      placeMark('4');
+      break;
+    case $('#5').text() !== player && $('#5').text() !== computer:
+      placeMark('5');
+      break;
+     case $('#6').text() !== player && $('#6').text() !== computer:
+      placeMark('6');
+      break;
+    case $('#7').text() !== player && $('#7').text() !== computer:
+      placeMark('7');
+      break;
+    case $('#8').text() !== player && $('#8').text() !== computer:
+      placeMark('8');
+      break;
+    case $('#9').text() !== player && $('#9').text() !== computer:
+      placeMark('9');
+      break;
+  }
+}
